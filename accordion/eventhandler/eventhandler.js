5c08c7642d7d96311ebf7caaa5190428d7ad7159
hapi <arpad.bajzath@intren.hu>
2015-05-11 22:02:39 +0200

    OMG I always forget something... :-D

--- accordion/eventhandler/eventhandler.js
+++ accordion/eventhandler/eventhandler.js
@@ -1,5 +1,7 @@
-document.querySelector('body').addEventListener('click', function(event) {
-  if (event.target.className === "toggle-panel") {
-    event.target.parentNode.parentNode.classList.toggle('active');
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('body').addEventListener('click', function(event) {
+    if (event.target.className === "toggle-panel") {
+      event.target.parentNode.parentNode.classList.toggle('active');
+    }
+  });
 });
